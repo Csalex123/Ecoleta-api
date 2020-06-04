691f4df720e127270fd70c55502c26fb073993bd
Alex Ricardo <alex.ricardo1999@hotmail.com>
2020-06-03 22:43:33 -0300

    Creation of tables (Points and Items)

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,11 +1,11 @@
-import Knex from 'knex';
+import knex from 'knex';
 import path from 'path';
-
-const connection = Knex({
-    client: 'sqlite3',
+const connection = knex({
+    client : 'sqlite3',
     connection: {
         filename: path.resolve(__dirname, 'database.sqlite'),
     },
-})
+    useNullAsDefault: true,
+});
 
 export default connection;
